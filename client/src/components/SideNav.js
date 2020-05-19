b80d4e3a7d65a239da659f419d609440bd0b89da
Evandy <andi_evandy@yahoo.co.id>
2020-05-19 15:48:15 +0700

    add total asset to sidebar

--- client/src/components/SideNav.js
+++ client/src/components/SideNav.js
@@ -1,16 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import "../css/nav.css";
 import logo from "../assets/029-purse.png";
+import { useSelector } from 'react-redux';
 
 export default function Side() {
+  const { plans } = useSelector(state => state);
+  const [assets, setAssets] = useState('');
+
+  function convertToRupiah(angka) {
+    return Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(angka);
+  }
+
+  useEffect(() => {
+    setAssets(plans.reduce((prev, plan) => {
+      switch (String(plan.type).toLowerCase()) {
+        case 'debit':
+          prev += Number(plan.amount);
+          break;
+        case 'credit':
+          prev -= Number(plan.amount);
+          break;
+      }
+      return prev;
+    }, 0));
+  }, [plans])
+
   return (
     <>
       <Nav className="d-none d-md-block sidebar" activeKey="/home">
         <div className="sidebar-sticky"></div>
         <div className="imgLogo">
           <img src={logo} className="img-fluid" alt="logo" />
+        </div>
+        <div className="total-assets">
+          <div className="total-assets-title">Assets</div>
+          <div className="total-assets-amount" style={{color: assets < 0 ? '#ff0000' : null }}>
+            {convertToRupiah(assets)}
+          </div>
         </div>
         <Nav.Item>
           <LinkContainer to="/">
